decc9e4db7502130d6c2138799f314223edf7204
David <davsschwartz@yahoo.com>
2024-02-11 12:58:03 +0200

    feat: get particles working again by migrating packages

--- src/components/header.js
+++ src/components/header.js
@@ -1,97 +1,112 @@
 import React, { useState, useEffect } from "react"
 import AnimatedLetter from "./animated-letter"
-import ParticlesComp from "./particles";
+import ParticlesComp from "./particles"
 import SecondaryHeader from "./secondary-header"
 
-
 const Header = () => {
-  const [ amountOfPressedLetters, setAmountOfPressedLetters ] = useState(0);
-  const [ isNameGlowing, setIsNameGlowing ] = useState(false);
-  const [ fallingText, setFallingText ] = useState('');
-  const [ flippingText, setFlippingText ] = useState('');
-  const [ spinningText, setSpinningText ] = useState('');
-  const [ poppingText, setPoppingText ] = useState('');
-  const [ animatedName, setAnimatedName ] = useState('');
-  const Name = "David";
+  const [amountOfPressedLetters, setAmountOfPressedLetters] = useState(0)
+  const [isNameGlowing, setIsNameGlowing] = useState(false)
+  const [fallingText, setFallingText] = useState("")
+  const [flippingText, setFlippingText] = useState("")
+  const [spinningText, setSpinningText] = useState("")
+  const [poppingText, setPoppingText] = useState("")
+  const [animatedName, setAnimatedName] = useState("")
+  const [enableParticleMovement, setEnableParticleMovement] = useState(false)
+
+  const Name = "David"
 
   useEffect(() => {
-    const Hi = 'Hi,'
-    setFallingText(buildTextAnimations(Hi, 'falling', 0));
-    const Im = `I'm`;
-    setFlippingText(buildTextAnimations(Im, 'flipping', Hi.length * 1.7));
-    const Comma = ',';
-    setSpinningText(buildTextAnimations(Comma, 'spinning', (Hi.length + Im.length + Name.length) * 1.7));
-    const Developer = 'web developer';
-    setPoppingText(buildTextAnimations(Developer, 'popping', (Hi.length + Im.length + Name.length + Comma.length) * 1.8));
-    setAnimatedName(updateAnimatedName());
-  },[]);
+    const Hi = "Hi,"
+    setFallingText(buildTextAnimations(Hi, "falling", 0))
+    const Im = `I'm`
+    setFlippingText(buildTextAnimations(Im, "flipping", Hi.length * 1.7))
+    const Comma = ","
+    setSpinningText(
+      buildTextAnimations(
+        Comma,
+        "spinning",
+        (Hi.length + Im.length + Name.length) * 1.7
+      )
+    )
+    const Developer = "web developer"
+    setPoppingText(
+      buildTextAnimations(
+        Developer,
+        "popping",
+        (Hi.length + Im.length + Name.length + Comma.length) * 1.8
+      )
+    )
+    setAnimatedName(updateAnimatedName())
+    setTimeout(() => {
+      setEnableParticleMovement(true)
+    }, 4000)
+  }, [])
 
-  const updateAnimatedName = (glowClass)=> {
-    return [...Name].map(
-      letter => <AnimatedLetter 
-                  key={letter} 
-                  letter={letter} 
-                  trackClick={setAmountOfPressedLetters} 
-                  amount={amountOfPressedLetters}
-                  glowClass={glowClass}
-                />
-    );
+  const updateAnimatedName = glowClass => {
+    return [...Name].map(letter => (
+      <AnimatedLetter
+        key={letter}
+        letter={letter}
+        trackClick={setAmountOfPressedLetters}
+        amount={amountOfPressedLetters}
+        glowClass={glowClass}
+      />
+    ))
   }
 
   useEffect(() => {
+    if (amountOfPressedLetters === 0) {
+      return;
+    }
     if (amountOfPressedLetters === Name.length) {
-      setAnimatedName(updateAnimatedName('glow'));
-      setIsNameGlowing(true);
+      setAnimatedName(updateAnimatedName("glow"))
+      setEnableParticleMovement(false)
+    } else {
+      setAnimatedName(updateAnimatedName(""))
+      setEnableParticleMovement(true)
     }
-    else{
-      setAnimatedName(updateAnimatedName(''));
-      setIsNameGlowing(false);
-    }
-}, [amountOfPressedLetters]);
-                          
+  }, [amountOfPressedLetters])
+
   return (
-    <header className={'main-header'}>
-      <div className={'header-container'}>
-        <h1 className={'header-text'}>
-            <p className={'mobile-margin-half'}>
-              {fallingText}
-            </p>
-            <p className={'h_min-width-max-cont mobile-margin-half'}>
-              {flippingText}
-              <span 
-                style={{
-                  marginLeft:'10px',
-                }}
-              >
-                {animatedName}
-              </span>
-              {spinningText}
-            </p>
-            <p className={'h_min-width-max-cont'}>
-              {poppingText}
-            </p>
+    <header className={"main-header"}>
+      <div className={"header-container"}>
+        <h1 className={"header-text"}>
+          <p className={"mobile-margin-half"}>{fallingText}</p>
+          <p className={"h_min-width-max-cont mobile-margin-half"}>
+            {flippingText}
+            <span
+              style={{
+                marginLeft: "10px",
+              }}
+            >
+              {animatedName}
+            </span>
+            {spinningText}
+          </p>
+          <p className={"h_min-width-max-cont"}>{poppingText}</p>
         </h1>
         <SecondaryHeader />
       </div>
-      <ParticlesComp enableParticlesMovement={!isNameGlowing}/>
+      <ParticlesComp enableParticlesMovement={enableParticleMovement} />
     </header>
-)
+  )
 }
 
 export default Header
 
 const buildTextAnimations = (text, className, timing) => {
-  return text.split('').map((char, index) => {
-      let style = {'animationDelay': ((index + timing) / 10) + 's'};
-      if (char === ' ')
-        style.marginLeft = '12px';
-      return <span
-              aria-hidden='true'
-              key={index}
-              style={style}
-              className={`${className}-text`}
-            >
-              {char}
-            </span>
-  });
-};
+  return text.split("").map((char, index) => {
+    let style = { animationDelay: (index + timing) / 10 + "s" }
+    if (char === " ") style.marginLeft = "12px"
+    return (
+      <span
+        aria-hidden="true"
+        key={index}
+        style={style}
+        className={`${className}-text`}
+      >
+        {char}
+      </span>
+    )
+  })
+}
--- src/components/particles.js
+++ src/components/particles.js
@@ -1,62 +1,65 @@
-import React, { useCallback } from "react"
-import Particles from "react-particles"
-import { loadFull } from "tsparticles"
+import React, { useCallback, useEffect, useState } from "react"
+import Particles, { initParticlesEngine } from "@tsparticles/react"
+import { loadSlim } from "@tsparticles/slim"
 
 const ParticlesComp = ({ enableParticlesMovement = true }) => {
-  const particlesInit = useCallback(async engine => {
-    console.log(engine)
-    await loadFull(engine)
+  const [init, setInit] = useState(false)
+
+  useEffect(() => {
+    initParticlesEngine(async engine => {
+      await loadSlim(engine)
+    }).then(() => {
+      setInit(true)
+    })
   }, [])
 
-  const particlesLoaded = useCallback(async container => {
-    await console.log(container)
-  }, [])
+  const particlesLoaded = container => {
+    console.log(container)
+  }
 
   return (
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      className="particles"
-      params={{
-        fullScreen: { enable: false, zIndex: 0 },
-        particles: {
-          links: {
-            enable: true,
-            color: "#fff",
-          },
-          number: {
-            value: 15,
-          },
-          size: {
-            value: 1,
-          },
-          move: {
-            enable: enableParticlesMovement,
-            outMode: "destroy",
-          },
-        },
-        interactivity: {
-          events: {
-            onhover: {
-              enable: true,
-              mode: "trail",
+    <>
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          className="particles"
+          options={{
+            detectRetina: true,
+            fullScreen: { enable: false, zIndex: 0 },
+            particles: {
+              links: {
+                enable: true,
+                color: "#fff",
+              },
+              number: {
+                value: 15,
+              },
+              size: {
+                value: 1,
+              },
+              move: {
+                enable: enableParticlesMovement,
+                outModes: "destroy",
+              },
             },
-            onClick: {
-              enable: true,
-              mode: "repulse",
+            interactivity: {
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+              },
+              modes: {},
             },
-          },
-          modes: {
-            trail: {
-              delay: 0.005,
-              quantity: 1,
-              pauseOnStop: true,
-            },
-          },
-        },
-      }}
-    />
+          }}
+        />
+      )}
+    </>
   )
 }
 
